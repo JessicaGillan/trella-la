610d631adf66987dabea7f02022e725fe2a14a66
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-02-03 12:17:47 -0500

    Configure Devise to respond to json

--- config/application.rb
+++ config/application.rb
@@ -10,16 +10,15 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Govna
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    config.to_prepare do
+      DeviseController.respond_to :html, :json
+    end
+
   end
 end
