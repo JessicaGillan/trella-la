14b75b46a896478608a15a345e069db9fc967420
Jessica G <gillan.jessica@gmail.com>
2017-02-06 17:00:27 -0700

    add modal pop up for cards

--- app/assets/javascripts/controllers/ShowCtrl.js
+++ app/assets/javascripts/controllers/ShowCtrl.js
@@ -2,6 +2,16 @@
 App.controller('ShowCtrl',
   ['$scope', '$state', '$stateParams', 'currentUser', 'BoardService', 'ListService',
   function($scope, $state, $stateParams, currentUser, BoardService, ListService) {
+    $('#cardModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget); // Button that triggered the modal
+      var card = button.data('card'); // Extract info from data-* attributes
+
+      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+      var modal = $(this);
+      modal.find('.modal-title').text(card.title);
+      modal.find('.modal-description').text(card.description);
+    })
 
     $scope.currentUser = currentUser;
 
